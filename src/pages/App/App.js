4d387e40f61f3788a017efc4cb21559b8e734d6d
Sung Kim <stwkim103@gmail.com>
2023-05-13 20:01:53 -0400

    Adds signup and loginforms

---
+++ src/pages/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from '../HomePage/HomePage'; 
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
