625db7114ad184b9b4ba9bd0df50163beb596dbb
dangabrielle <hoangabrielle@gmail.com>
2023-05-15 08:54:27 -1000

    debug main page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,13 @@
-import SignUpForm from '../../components/SignUpForm/SignUpForm';
-import LoginForm from '../../components/LoginForm/LoginForm';
-import { useState } from 'react';
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
+// import { useState } from "react";
 
 export default function HomePage({ setUser }) {
-    return (
-        <main>
-            <h1>Home Page</h1>
-            <SignUpForm setUser={setUser}/>
-            <LoginForm setUser={setUser}/>
-        </main>
-    );
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <SignUpForm setUser={setUser} />
+      <LoginForm setUser={setUser} />
+    </main>
+  );
 }
-
