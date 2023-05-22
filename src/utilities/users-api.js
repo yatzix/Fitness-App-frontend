f41bf005a2ebe08a746acad409a87bf9a788cf40
yatzix <gutierrezyatzil@gmail.com>
2023-05-22 16:21:34 -0700

    removes proxy and fixes baseURL

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,5 +1,5 @@
 import sendRequest from "./send-request";
-const BASE_URL = "/api/users";
+const BASE_URL = "https://sleepy-meadow-61708.herokuapp.com/api/users";
 
 export async function signUp(userData) {
   // passes in formData and sends a post req to the servers
