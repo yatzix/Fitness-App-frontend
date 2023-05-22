c20cf0a0142f07ae1c78af522155848903bfafc8
yatzix <gutierrezyatzil@gmail.com>
2023-05-22 16:30:59 -0700

    fixes fetch in workoutitems

--- src/components/WorkOutItems/WorkOutItems.js
+++ src/components/WorkOutItems/WorkOutItems.js
@@ -41,7 +41,7 @@ const WorkOutItems = ({ user, onAddExercise, workouts }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://sleepy-meadow-61708.herokuapp.com/api/data", {
+      const response = await fetch(`https://sleepy-meadow-61708.herokuapp.com/api/exercises?muscle=${muscle}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
