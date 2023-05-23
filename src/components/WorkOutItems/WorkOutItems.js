5cf457901d0fac543e19443ad628258963a0bf23
yatzix <gutierrezyatzil@gmail.com>
2023-05-22 18:32:51 -0700

    debug

--- src/components/WorkOutItems/WorkOutItems.js
+++ src/components/WorkOutItems/WorkOutItems.js
@@ -1,62 +1,52 @@
 import React, { useEffect, useState } from "react";
 import ExerciseList from "../ExerciseList/ExerciseList";
-import './WorkOutItems.css'
-
+import styles from "./WorkOutItems.css";
 const WorkOutItems = ({ user, onAddExercise, workouts }) => {
   const [data, setData] = useState(null);
-
   const [muscle, setMuscle] = useState("");
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const api_key = process.env.REACT_APP_API_KEY;
         const api_url =
-        `https://sleepy-meadow-61708.herokuapp.com/api/exercises?muscle=${muscle}`;
-        const response = await fetch(api_url, {
-          headers: {
-            "X-Api-Key": api_key,
-          },
-        });
-
+          "https://sleepy-meadow-61708.herokuapp.com/api/users/data";
+        const response = await fetch(api_url);
         if (response.ok) {
           const responseData = await response.json();
           setData(responseData);
-        } else {
-          throw new Error("Request failed");
+          // } else {
+          // throw new Error("Request failed");
         }
       } catch (error) {
         console.error(error);
       }
     };
-
     fetchData();
   }, [muscle]);
-
   const handleMuscleChange = (event) => {
     setMuscle(event.target.value);
   };
-
   const handleSubmit = async (e, exercise) => {
     e.preventDefault();
-
     try {
-      const response = await fetch("https://sleepy-meadow-61708.herokuapp.com/api/data", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          user: user._id,
-        },
-        body: JSON.stringify({
-          name: exercise.name,
-          type: exercise.type,
-          muscle: exercise.muscle,
-          equipment: exercise.equipment,
-          difficulty: exercise.difficulty,
-          instructions: exercise.instructions,
-        }),
-      });
-
+      const response = await fetch(
+        "https://sleepy-meadow-61708.herokuapp.com/api/data",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            user: user._id,
+          },
+          body: JSON.stringify({
+            name: exercise.name,
+            type: exercise.type,
+            muscle: exercise.muscle,
+            equipment: exercise.equipment,
+            difficulty: exercise.difficulty,
+            instructions: exercise.instructions,
+          }),
+        }
+      );
       if (response.ok) {
         console.log("Data submitted to MongoDB");
         onAddExercise(exercise); // Add the exercise to the parent component's workouts state
@@ -67,11 +57,9 @@ const WorkOutItems = ({ user, onAddExercise, workouts }) => {
       console.error("Error submitting data to MongoDB:", error);
     }
   };
-
   if (!data) {
     return <p>Loading...</p>;
   }
-
   return (
     <div>
       <div id="main-container">
@@ -79,7 +67,6 @@ const WorkOutItems = ({ user, onAddExercise, workouts }) => {
           <label htmlFor="muscle-input" className="display-txt">
             Search by muscle:
           </label>
-
           <input
             id="muscle-input"
             type="text"
@@ -106,49 +93,45 @@ const WorkOutItems = ({ user, onAddExercise, workouts }) => {
             <li>triceps</li>
           </ul>
         </div>
-
         <div className="scroll">
           <h1 className="display-txt">Exercises:</h1>
           <ol>
             {data.map((exercise) => (
               <li key={exercise.id}>
-              <div className="exercise-box">
-                <h3 className="exercise-name">{exercise.name}</h3>
-                <p className="type">
-                  <strong>Type:</strong> {exercise.type}
-                </p>
-                <p className="muscle">
-                  <strong>Muscle:</strong> {exercise.muscle}
-                </p>
-                <p className="equipment">
-                  <strong>Equipment:</strong> {exercise.equipment}
-                </p>
-                <p className="difficulty">
-                  <strong>Difficulty:</strong> {exercise.difficulty}
-                </p>
-                <p className="instructions">
-                  <strong>Instructions:</strong> {exercise.instructions}
-                </p> 
-                 <form
-                 className="input-container"
-                  autoComplete="off"
-                  onSubmit={(e) => handleSubmit(e, exercise)}
-                >
-                  <input
-                    type="submit"
-                    name="workouts"
-                    value="Fit Add"
-                    readOnly
-                  />
-                  
-                </form> 
+                <div className="exercise-box">
+                  <h3 className="exercise-name">{exercise.name}</h3>
+                  <p className="type">
+                    <strong>Type:</strong> {exercise.type}
+                  </p>
+                  <p className="muscle">
+                    <strong>Muscle:</strong> {exercise.muscle}
+                  </p>
+                  <p className="equipment">
+                    <strong>Equipment:</strong> {exercise.equipment}
+                  </p>
+                  <p className="difficulty">
+                    <strong>Difficulty:</strong> {exercise.difficulty}
+                  </p>
+                  <p className="instructions">
+                    <strong>Instructions:</strong> {exercise.instructions}
+                  </p>
+                  <form
+                    className="input-container"
+                    autoComplete="off"
+                    onSubmit={(e) => handleSubmit(e, exercise)}
+                  >
+                    <input
+                      type="submit"
+                      name="workouts"
+                      value="Fit Add"
+                      readOnly
+                    />
+                  </form>
                 </div>
                 <br></br>
               </li>
-             
             ))}
           </ol>
-          
         </div>
         <div>
           <ExerciseList workouts={workouts} />
@@ -157,5 +140,4 @@ const WorkOutItems = ({ user, onAddExercise, workouts }) => {
     </div>
   );
 };
-
 export default WorkOutItems;
